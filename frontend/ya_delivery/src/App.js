2385781a6127617fa035e71dfabc17197f670ddd
VikaGenius <v.stepanova3@g.nsu.ru>
2024-05-06 22:58:33 +0700

    Add paths to router in App

--- frontend/ya_delivery/src/App.js
+++ frontend/ya_delivery/src/App.js
@@ -2,6 +2,11 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LoginForm from './Components/LoginForm/LoginForm'; 
 import RegistrationForm from './Components/RegistrationForm/RegistrationForm'; 
 import React from 'react';
+import GeolocationPage from '../LocationTest/Location';
+import Main from '../Main/Main';
+import ShopPage from '../ShopPage/ShopPage';
+import Cart from '../Cart/Cart';
+import AccountPage from '../AccountPage/AccountPage';
 
 import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 
@@ -19,6 +24,21 @@ const App = () => {
             <Routes>
             <Route path="login" element={<LoginForm />} />
             <Route path="register" element={<RegistrationForm />} />
+
+            <Route path="/" element= {<Main/>}/>  
+            <Route path="main" element= {<Main/>}/>  
+            <Route path="geo" element= {<GeolocationPage/>}/>
+
+
+            <Route path="shops/:id" element={<ShopPage/>}/>
+            <Route path="main/shops/:id" element={<ShopPage/>}/>
+
+            <Route path="cart" element={<Cart/>} /> 
+            <Route path="main/cart" element={<Cart/>} /> 
+
+            <Route path="account" element={<AccountPage/>} /> 
+            <Route path="main/account" element={<AccountPage/>} /> 
+
             </Routes>
         </BrowserRouter>        
         </ApolloProvider>
