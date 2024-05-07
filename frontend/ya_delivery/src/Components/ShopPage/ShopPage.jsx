1b3871d7ad6ff4ff17110fc895999c06c1020147
VikaGenius <v.stepanova3@g.nsu.ru>
2024-05-07 23:56:22 +0700

    Add style for ShopPage

--- frontend/ya_delivery/src/Components/ShopPage/ShopPage.jsx
+++ frontend/ya_delivery/src/Components/ShopPage/ShopPage.jsx
@@ -40,14 +40,43 @@ const ShopPage = () => {
 
   return (
     <div>
-      <div>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column", // добавлено для вертикального выравнивания элементов списка
+          backgroundColor: "#333", // фон всего контейнера
+          borderRadius: "8px",
+          padding: "20px", // добавлено немного внутреннего отступа для визуального улучшения
+        }}
+      >
         <h2>Products:</h2>
-        <ul>
+        <ul
+          
+        >
           {data.GetProducts.map((product) => (
-            <li key={product.idPriduct}>
+            <li 
+              style={{
+                backgroundColor: "#444", // фон каждой карточки продукта
+                borderRadius: "8px", // скругление углов каждой карточки
+                margin: "10px", // отступы между карточками
+                padding: "10px", // внутренние отступы карточек
+                color: '#FFF' // цвет текста в карточке, чтобы лучше читался на темном фоне
+              }}
+              key={product.idPriduct}>
               <h3>{product.name}</h3>
               <p>Cost: {product.cost}</p>
-              <button>Add to Cart</button>
+              <button
+                style={{ 
+                  backgroundColor: '#FFF', // фон кнопки
+                  color: '#333', // цвет текста кнопки
+                  border: 'none', // убрать стандартный бордер кнопки
+                  padding: '5px 10px', // паддинги кнопки
+                  borderRadius: '5px', // скругление углов кнопки
+                  cursor: 'pointer', // курсор в виде указателя
+                  marginTop: '10px'
+                }}
+              >Add to Cart</button>
             </li>
           ))}
         </ul>
