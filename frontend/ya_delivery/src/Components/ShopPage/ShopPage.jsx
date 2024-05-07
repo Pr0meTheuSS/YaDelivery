1207f6c3d30544ed63b50ccf8db63711b2518781
yolimpiev <yr.olimpiev@gmail.com>
2024-05-07 22:10:18 +0700

    Add products list

--- frontend/ya_delivery/src/Components/ShopPage/ShopPage.jsx
+++ frontend/ya_delivery/src/Components/ShopPage/ShopPage.jsx
@@ -1,10 +1,59 @@
-import 'react-dom';
 import React from 'react';
+import { useQuery, gql } from '@apollo/client';
 import { useParams } from 'react-router-dom';
 
-const ShopPage = (props) => {
-    const {id} = useParams();
-    return <h1>Тут будет страница с данными о магазине {id}</h1>
-} 
+// const GET_SHOP = gql`
+//   query GetShop($shopId: ID!) {
+//     shop(id: $shopId) {
+//       id
+//       name
+//       image
+//       address
+//       geoPos {
+//         latitude
+//         longitude
+//       }
+//       info
+//     }
+//   }
+// `;
+
+const getProducts = gql`
+  query GetProducts($idShop: ID!) {
+    GetProducts(idShop: $idShop) {
+      idPriduct
+      name
+      cost
+    }
+  }
+`;
+
+const ShopPage = () => {
+  const { id } = useParams(0);
+
+  const { loading, error, data } = useQuery(getProducts, {
+    variables: { idShop: id },
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  return (
+    <div>
+      <div>
+        <h2>Products:</h2>
+        <ul>
+          {data.GetProducts.map((product) => (
+            <li key={product.idPriduct}>
+              <h3>{product.name}</h3>
+              <p>Cost: {product.cost}</p>
+              <button>Add to Cart</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default ShopPage;
