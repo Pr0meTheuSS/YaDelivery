698004e883b9570aabf9e359d30535e49dff9466
yolimpiev <yr.olimpiev@gmail.com>
2024-06-03 23:58:57 +0700

    Add Dockerfile, fix http2 frame size error

--- backend/gateway/internal/api/graph/resolver.go
+++ backend/gateway/internal/api/graph/resolver.go
@@ -1,10 +1,15 @@
 package graph
 
 import (
+	"fmt"
+	"gateway/internal/config"
 	organizationService "gateway/internal/services/organization_service"
 	productService "gateway/internal/services/product_service"
 	shopService "gateway/internal/services/shop_service"
 	userservice "gateway/internal/services/user_service"
+	"log"
+
+	"github.com/caarlos0/env"
 )
 
 // This file will not be regenerated automatically.
@@ -19,8 +24,16 @@ type Resolver struct {
 }
 
 func NewResolver() *Resolver {
+	cfg := config.Config{} // 👈 new instance of `Config`
+
+	err := env.Parse(&cfg) // 👈 Parse environment variables into `Config`
+	if err != nil {
+		log.Fatalf("unable to parse ennvironment variables: %e", err)
+	}
+
+	fmt.Printf("Auth service: %s:%d\n", cfg.AuthServiceHost, cfg.AuthServicePort)
 	return &Resolver{
-		userService:    userservice.NewUserService(),
+		userService:    userservice.NewUserService(cfg.AuthServiceHost, int32(cfg.AuthServicePort)),
 		shopService:    shopService.NewShopService(),
 		orgService:     organizationService.NewOrganizationService(),
 		productService: productService.NewProductService(),
